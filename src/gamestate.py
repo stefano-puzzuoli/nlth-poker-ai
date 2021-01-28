9b5b974537e8f7027ebe5a8205b4285d0ab0ef79
puzzuos2 <stefano.puzzuoli2@mail.dcu.ie>
2021-01-28 19:03:02 +0000

    Added GameState class to keep track of ongoing game

---
+++ src/gamestate.py
@@ -0,0 +1,21 @@
+class GameState:
+
+    def __init__(self, players):
+
+        self.to_call = None                           
+        
+        self.min_raise = None                         
+        
+        self.num_players = len(players)               
+        
+        self.bets = [0 for p in players]       
+
+        self.current_bets = [0 for p in players]   
+        
+        self.folded = []
+        
+        self.all_in = []                                    
+        
+        self.cards = []                              
+        
+        self.actor = None                             
