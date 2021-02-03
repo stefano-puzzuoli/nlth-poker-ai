4c5df4f02832411c70a23566140567a3b584ded2
puzzuos2 <stefano.puzzuoli2@mail.dcu.ie>
2021-02-03 18:08:29 +0000

    Added functional component to allow new user Signup

---
+++ src/ui/src/components/Signup.js
@@ -0,0 +1,57 @@
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Container, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+import Logo from './Logo';
+import '../Poker.css';
+
+export default function Signup() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+  const { signup } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+
+  return (
+    <>
+      <Container
+        className="d-flex flex-column align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-200 text-center">
+            <Logo/>
+        </div>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Sign Up</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
+                <Form.Group id="password-confirm">
+                  <Form.Label>Password Confirmation</Form.Label>
+                  <Form.Control type="password" ref={passwordConfirmRef} required />
+                </Form.Group>
+                <Button disabled={loading} className="w-100" type="submit">
+                  Sign Up
+            </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            Already have an account? <Link to="/login">Log In</Link>
+          </div>
+        </div>
+      </Container>
+    </>
+  )
+}
