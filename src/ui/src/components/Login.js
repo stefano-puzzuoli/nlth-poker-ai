eb925915163d44408e5b5d0177e6161d9b987fe3
idelegi2 <immanuel.idelegbagbon2@mail.dcu.ie>
2021-02-04 01:03:40 +0000

    Added javascript file to handle user login.

---
+++ src/ui/src/components/Login.js
@@ -0,0 +1,88 @@
+import React, { useRef, useState } from "react"
+import { Form, Button, Card, Container, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
+import { Link, useHistory } from "react-router-dom"
+import Logo from './Logo';
+import '../Poker.css';
+
+export default function Login() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const { login } = useAuth()
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const history = useHistory()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    try {
+      setError("")
+      setLoading(true)
+      await login(emailRef.current.value, passwordRef.current.value)
+      history.push("/dashboard")
+    } catch {
+      setError("Invalid email or password")
+    }
+
+    setLoading(false)
+  }
+
+  async function handleSubmitGuest(e) {
+    e.preventDefault()
+    console.log("Guest log in")
+    try {
+      setError("")
+      setLoading(true)
+      await login("guest@guest.com", "password")
+      history.push("/dashboard")
+    } catch {
+      setError("Error. Cannot play as guest")
+    }
+
+    setLoading(false)
+  }
+
+  return (
+    <>
+      <Container
+        className="d-flex flex-column align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-200 text-center">
+            <Logo/>
+        </div>
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Log In</h2>
+              {error && <Alert variant="danger">{error}</Alert>}
+              <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
+                <Button disabled={loading} className="w-100" type="submit">
+                  Log In
+            </Button>
+              </Form>
+              <Button disabled={loading} variant="warning" className="w-100 mt-3" onClick={handleSubmitGuest}>
+                  Play as Guest
+            </Button>
+              <div className="w-100 text-center mt-3">
+                <Link to="/forgot-password">Forgot Password?</Link>
+              </div>
+            </Card.Body>
+          </Card>
+          <div className="w-100 text-center mt-2">
+            Don't have an account? <Link to="/signup">Sign Up</Link>
+          </div>
+        </div>
+      </Container>
+    </>
+  )
+}
