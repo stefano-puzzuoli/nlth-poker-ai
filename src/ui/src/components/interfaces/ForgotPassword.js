4807e332ab8a5c44d7cfba0113990a16f66538b7
puzzuos2 <stefano.puzzuoli2@mail.dcu.ie>
2021-02-20 21:18:04 +0000

    Moved and added comments to ForgotPassword component

--- src/ui/src/components/interfaces/ForgotPassword.js
+++ src/ui/src/components/interfaces/ForgotPassword.js
@@ -1,8 +1,12 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Container, Alert } from "react-bootstrap"
-import { useAuth } from "../contexts/AuthContext"
+import { useAuth } from "../../contexts/AuthContext"
 import { Link } from "react-router-dom"
 
+/**
+ * ForgotPassword component to allow users 
+ * to reset their account password (needed to login).
+ */
 export default function ForgotPassword() {
   const emailRef = useRef()
   const { resetPassword } = useAuth()
