b1a37c5b4aa26219671c15a3d5e584d7be394635
puzzuos2 <stefano.puzzuoli2@mail.dcu.ie>
2021-02-23 19:49:03 +0000

    Changed play_game.py to game.py

--- src/ai_poker/tests/AI_model_test.py
+++ src/ai_poker/tests/AI_model_test.py
@@ -1,8 +1,4 @@
 import unittest
-import pandas as pd
-import numpy as np
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LinearRegression
 from pandas.testing import assert_frame_equal     # for testing dataframes
 from pandas.testing import assert_series_equal    # for testing series
 import sys
@@ -11,7 +7,7 @@ from table import Table
 
 
 class AI_model_test(unittest.TestCase):
-    ''' Class for running unittests on functionalities of play_game.py '''
+    ''' Class for running unittests on functionalities of game.py '''
 
     def setUp(self):
         ''' SetUp dataframe '''
@@ -24,7 +20,7 @@ class AI_model_test(unittest.TestCase):
 
 
 def main():
-    df = Prediction_model_test()
+    df = AI_model_test()
     df.setUp()
 
 if __name__ == "__main__":
