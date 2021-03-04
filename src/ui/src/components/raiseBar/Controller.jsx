886d8b88a972120c67aa58affe2dc6322c792fa6
idelegi2 <immanuel.idelegbagbon2@mail.dcu.ie>
2021-03-04 13:11:36 +0000

    Added comments to Controller function

--- src/ui/src/components/raiseBar/Controller.jsx
+++ src/ui/src/components/raiseBar/Controller.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+/**
+ * Controller functional component which represents the base 
+ * of the raise bar users can perform  call, fold or raise
+ * actions through.
+ */
 function Controller({
   handle: { id, value, percent },
   getHandleProps
