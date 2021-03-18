f73973261b93f6ab7c79b4f8d4aa6df4a3c14376
idelegi2 <immanuel.idelegbagbon2@mail.dcu.ie>
2021-03-18 00:33:50 +0000

    Refactored bet service

--- src/ui/src/services/betService.js
+++ src/ui/src/services/betService.js
@@ -7,29 +7,39 @@ import { chooseNextActivePlayer } from './playersService.js';
 * relating to bets and the betting pot.
 */
 
-const calculateBlindIndices = (dealerIndex, numPlayers) => {
-	return ({
-		bigBlindIndex: (dealerIndex + 2) % numPlayers,
-		smallBlindIndex: (dealerIndex + 1) % numPlayers,
-	});
-}
-
-const anteUpBlinds = (players, blindIndices, minBet) => {
-	const { bigBlindIndex, smallBlindIndex } = blindIndices;
-	players[bigBlindIndex].bet = minBet;
-	players[bigBlindIndex].chips = players[bigBlindIndex].chips - minBet;
-	players[smallBlindIndex].bet = minBet / 2;
-	players[smallBlindIndex].chips = players[smallBlindIndex].chips - (minBet / 2);
-	return players
-}
-
-const calculateMinBet = (highBet, playerChipsStack, playerBet) => {
-	const playerTotalChips = playerChipsStack + playerBet
-	if (playerTotalChips < highBet) {
-		return playerTotalChips;
-	} else {
-		return highBet;
+/* 
+ Determine which players are included in the current pot.
+*/
+const calculatePots = (state, playerStacks) => {
+	// remove players who did not bet from pot
+	const commitedPlayers = playerStacks.filter(player => player.sidePotStack > 0)
+	if (commitedPlayers.length === 0) {
+		return state
 	}
+	else if (commitedPlayers.length === 1) {
+		// refund players who bet in excess of pot
+		const playerToRefund = state.players[state.players.findIndex(player => player.name === commitedPlayers[0].name)];
+		playerToRefund.chips = playerToRefund.chips + commitedPlayers[0].sidePotStack;
+		state.pot -= commitedPlayers[0].sidePotStack
+		return state
+	}
+	// sort players
+	const ascBetPlayers = commitedPlayers.sort((a, b) => a.sidePotStack - b.sidePotStack);
+	const smallStackValue = ascBetPlayers[0].sidePotStack;
+
+	const builtSidePot = ascBetPlayers.reduce((acc, cur) => {
+		if (!cur.folded) {
+			acc.contestants.push(cur.name);
+		}
+		acc.potValue = acc.potValue + smallStackValue;
+		cur.sidePotStack = cur.sidePotStack - smallStackValue;
+		return acc
+	}, {
+		contestants: [],
+		potValue: 0,
+	});
+	state.sidePots.push(builtSidePot);
+	return calculatePots(state, ascBetPlayers)
 }
 
 /* 
@@ -55,7 +65,6 @@ const manageBet = (state, bet, min, max) => {
 			}
 		}
 	}
-
 	const activePlayer = state.players[state.activePlayerIndex];
 	const subtractableChips = bet - activePlayer.bet;
 	activePlayer.bet = bet;
@@ -67,17 +76,6 @@ const manageBet = (state, bet, min, max) => {
 	}
 	activePlayer.betReconciled = true;
 	return chooseNextActivePlayer(state)
-}
-
-const managePlayerFold = (state) => {
-	const activePlayer = state.players[state.activePlayerIndex];
-	activePlayer.folded = true;
-	activePlayer.betReconciled = true;
-	state.playersFolded++
-	state.playersActive--
-
-	const nextState = chooseNextActivePlayer(state)
-	return nextState
 }
 
 const managePhaseShift = (state) => {
@@ -100,6 +98,66 @@ const managePhaseShift = (state) => {
 		}
 		default: throw Error("managePhaseShift() called from non-betting phase")
 	}
+}
+
+const calculateMinBet = (highBet, playerChipsStack, playerBet) => {
+	const playerTotalChips = playerChipsStack + playerBet
+	if (playerTotalChips < highBet) {
+		return playerTotalChips;
+	} else {
+		return highBet;
+	}
+}
+
+const calculateBlindIndices = (dealerIndex, numPlayers) => {
+	return ({
+		bigBlindIndex: (dealerIndex + 2) % numPlayers,
+		smallBlindIndex: (dealerIndex + 1) % numPlayers,
+	});
+}
+
+const managePlayerFold = (state) => {
+	const activePlayer = state.players[state.activePlayerIndex];
+	activePlayer.folded = true;
+	activePlayer.betReconciled = true;
+	state.playersFolded++
+	state.playersActive--
+
+	const nextState = chooseNextActivePlayer(state)
+	return nextState
+}
+/* 
+ Condenses mutiple pots into a single pot. 
+*/
+const condensePots = (state) => {
+	if (state.sidePots.length > 1) {
+		for (let i = 0; i < state.sidePots.length; i++) {
+			for (let n = i + 1; n < state.sidePots.length; n++) {
+				if (arrayIdentical(state.sidePots[i].contestants, state.sidePots[n].contestants)) {
+					state.sidePots[i].potValue = state.sidePots[i].potValue + state.sidePots[n].potValue;
+					state.sidePots = state.sidePots.filter((el, index) => index !== n);
+				}
+			}
+		}
+	}
+	return state
+}
+
+const anteUpBlinds = (players, blindIndices, minBet) => {
+	const { bigBlindIndex, smallBlindIndex } = blindIndices;
+	players[bigBlindIndex].bet = minBet;
+	players[bigBlindIndex].chips = players[bigBlindIndex].chips - minBet;
+	players[smallBlindIndex].bet = minBet / 2;
+	players[smallBlindIndex].chips = players[smallBlindIndex].chips - (minBet / 2);
+	return players
+}
+
+const arrayIdentical = (arr1, arr2) => {
+
+	if (arr1.length !== arr2.length) {
+		return false
+	}
+	return arr1.map(el => arr2.includes(el)).filter(bool => bool !== true).length !== 0 ? false : true;
 }
 
 /* 
@@ -128,66 +186,6 @@ const remakePot = (state) => {
 	return state
 }
 
-/* 
- Determine which players are included in the current pot.
-*/
-const calculatePots = (state, playerStacks) => {
-	// remove players who did not bet from pot
-	const investedPlayers = playerStacks.filter(player => player.sidePotStack > 0)
-	if (investedPlayers.length === 0) {
-		return state
-	}
-	if (investedPlayers.length === 1) {
-		// refund players who bet in excess of pot
-		const playerToRefund = state.players[state.players.findIndex(player => player.name === investedPlayers[0].name)];
-		playerToRefund.chips = playerToRefund.chips + investedPlayers[0].sidePotStack;
-		state.pot -= investedPlayers[0].sidePotStack
-		return state
-	}
-	// sort players
-	const ascBetPlayers = investedPlayers.sort((a, b) => a.sidePotStack - b.sidePotStack);
-	const smallStackValue = ascBetPlayers[0].sidePotStack;
-
-	const builtSidePot = ascBetPlayers.reduce((acc, cur) => {
-		if (!cur.folded) {
-			acc.contestants.push(cur.name);
-		}
-		acc.potValue = acc.potValue + smallStackValue;
-		cur.sidePotStack = cur.sidePotStack - smallStackValue;
-		return acc
-	}, {
-		contestants: [],
-		potValue: 0,
-	});
-	state.sidePots.push(builtSidePot);
-	return calculatePots(state, ascBetPlayers)
-
-}
-
-/* 
- Condenses mutiple pots into a single pot. 
-*/
-const condensePots = (state) => {
-	if (state.sidePots.length > 1) {
-		for (let i = 0; i < state.sidePots.length; i++) {
-			for (let n = i + 1; n < state.sidePots.length; n++) {
-				if (arrayIdentical(state.sidePots[i].contestants, state.sidePots[n].contestants)) {
-					state.sidePots[i].potValue = state.sidePots[i].potValue + state.sidePots[n].potValue;
-					state.sidePots = state.sidePots.filter((el, index) => index !== n);
-				}
-			}
-		}
-	}
-	return state
-}
-
-const arrayIdentical = (arr1, arr2) => {
-
-	if (arr1.length !== arr2.length) {
-		return false
-	}
-	return arr1.map(el => arr2.includes(el)).filter(bool => bool !== true).length !== 0 ? false : true;
-}
 export {
 	calculateBlindIndices,
 	anteUpBlinds,
